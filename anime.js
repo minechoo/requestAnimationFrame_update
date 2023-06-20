ab8b9f926773aeaeb9f7137e241a4f8dc5c0a377
tubi55 <tubi55@nate.com>
2023-06-20 13:19:29 +0900

    progress를 통한 모션 진행 단계별 진행률 반환

--- anime.js
+++ anime.js
@@ -3,25 +3,36 @@
   1ms단위로 정밀한 시간계산이 가능
   브라우저가 로딩된 순간부터 해당 구문이 호출된 시점까지의 시간을 ms단위로 반환
   정밀한 시간계산이 필요할때 활용됨
+
+  특정시간동안 특정 수치값까지 변경
+  반복횟수(x), 고정된 반복횟수안에서 변화량을 제어 (0)
 */
+//1초동안 500px이동
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
-let num = 0;
+const speed = 1000;
+const targetValue = 500;
 let startTime = 0;
+let count = 0;
 
 btn.addEventListener('click', () => {
 	startTime = performance.now();
-	console.log('시작시간', startTime);
 	requestAnimationFrame(move);
 });
 
 function move(time) {
-	console.log('반복사이클 마다의 누적시간', time);
-	num++;
-	box.style.marginLeft = num + 'px';
-	if (num >= 60) {
-		console.log('총 모션 시간', time - startTime);
-		return;
+	//timelast : 각 사이클 마다 걸리는 누적시간
+	let timelast = time - startTime;
+
+	//매 반복횟수마다 현재 걸리는 누적시간값을 전체시간으로 나누면 0~1사이의 실수로 반환 가능
+	//progress : 설정한 시간되비 현재 반복되는 모션 진행상황을 0~1사이의 소수점으로 나타내주는 진행률 (x100 -백분율)
+	let progress = timelast / speed;
+	console.log('timelast', timelast);
+	console.log('progress', progress);
+
+	console.log('반복횟수', count++);
+
+	if (progress < 1) {
+		requestAnimationFrame(move);
 	}
-	requestAnimationFrame(move);
 }
