a1eb461d3d5e48b4b20aabcc5e046cd0ffe6beb5
tubi55 <tubi55@nate.com>
2023-06-21 10:07:51 +0900

    progress값 보정

--- anime.js
+++ anime.js
@@ -1,13 +1,3 @@
-/*
-  peformance.now();
-  1ms단위로 정밀한 시간계산이 가능
-  브라우저가 로딩된 순간부터 해당 구문이 호출된 시점까지의 시간을 ms단위로 반환
-  정밀한 시간계산이 필요할때 활용됨
-
-  특정시간동안 특정 수치값까지 변경
-  반복횟수(x), 고정된 반복횟수안에서 변화량을 제어 (0)
-*/
-//1초동안 500px이동
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
 
@@ -27,11 +17,14 @@ function anime(selector, option) {
 		let timelast = time - startTime;
 		let progress = timelast / option.duration;
 
+		//progress값이 시작이 음수로 떨어지거나 혹은 종료시 1이 넘어서는 경우를 각각 0, 1로 보정
+		//progress값이 적용되는 targetValue값도 딱 정수로 떨어짐 (px단위에서 중요함)
+		progress < 0 && (progress = 0);
+		progress > 1 && (progress = 1);
+		progress < 1 && requestAnimationFrame(move);
+
 		console.log('진행률', progress);
 
-		if (progress < 1) {
-			requestAnimationFrame(move);
-		}
 		selector.style[option.prop] = option.value * progress + 'px';
 	}
 }
