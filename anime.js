7b2adbf3616d44c560d38c90a4850832438a28e2
tubi55 <tubi55@nate.com>
2023-06-21 10:39:42 +0900

    현재 수치값보다 타겟값이 큰 경우 수치량이 증가하는 함수 추가

--- anime.js
+++ anime.js
@@ -4,33 +4,30 @@ const box = document.querySelector('#box');
 btn.addEventListener('click', () => {
 	anime(box, {
 		prop: 'margin-left',
-		value: 500,
+		value: 100,
 		duration: 500,
-		callback: () => console.log('end'),
 	});
 });
 
 function anime(selector, option) {
-	let startTime = performance.now();
-	requestAnimationFrame(move);
+	const startTime = performance.now();
 
-	function move(time) {
+	//selector요소의 현재 css에 적용되어 있는 값을 가져온뒤, parseInt를 활용해 숫자값으로 변경
+	const currentValue = parseInt(getComputedStyle(selector)[option.prop]);
+
+	if (option.value === currentValue) return;
+	if (option.value > currentValue) requestAnimationFrame(plus);
+	if (option.value < currentValue) requestAnimationFrame(minus);
+
+	function plus(time) {
 		let timelast = time - startTime;
 		let progress = timelast / option.duration;
 
-		//progress값이 시작이 음수로 떨어지거나 혹은 종료시 1이 넘어서는 경우를 각각 0, 1로 보정
-		//progress값이 적용되는 targetValue값도 딱 정수로 떨어짐 (px단위에서 중요함)
 		progress < 0 && (progress = 0);
 		progress > 1 && (progress = 1);
-		progress < 1 ? requestAnimationFrame(move) : option.callback && option.callback();
-		/*
-		if (progress < 1) {
-			requestAnimationFrame(move);
-		} else {
-			if(option.callback) option.callback();
-		}
-		*/
+		progress < 1 ? requestAnimationFrame(plus) : option.callback && option.callback();
 
-		selector.style[option.prop] = option.value * progress + 'px';
+		let result = currentValue + (option.value - currentValue) * progress;
+		selector.style[option.prop] = result + 'px';
 	}
 }
