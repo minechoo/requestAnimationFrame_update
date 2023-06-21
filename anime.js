43c32700e2771cd1d303220d264553e7798c7d6b
tubi55 <tubi55@nate.com>
2023-06-21 11:28:31 +0900

    속성값이 문자열일때 부모값을 기준으로 퍼센트 연산처리

--- anime.js
+++ anime.js
@@ -3,18 +3,42 @@ const box = document.querySelector('#box');
 
 btn.addEventListener('click', () => {
 	anime(box, {
-		prop: 'margin-left',
-		value: 500,
+		prop: 'margin-top',
+		value: '50%',
 		duration: 500,
 	});
 });
 
 function anime(selector, option) {
 	const startTime = performance.now();
+	let currentValue = parseInt(getComputedStyle(selector)[option.prop]);
 
-	//selector요소의 현재 css에 적용되어 있는 값을 가져온뒤, parseInt를 활용해 숫자값으로 변경
-	const currentValue = parseInt(getComputedStyle(selector)[option.prop]);
-	//option.value와 currentValue가 같지 않을때에만 모션처리
+	//만약 value속성으로 받은 값이 문자열이면 퍼센트연산처리 해야되므로 정수가 아닌 실수로 값을 변환
+	const isString = typeof option.value;
+	if (isString === 'string') {
+		//option.value값이 문자열이면 기존의 currentValue값도 퍼센트 처리
+		//퍼센트로 값을 변환하기 위해서는 부모요소의 전체넓이, 전체 높이값을 구함
+		const parentW = parseInt(getComputedStyle(selector.parentElement).width);
+		const parentH = parseInt(getComputedStyle(selector.parentElement).height);
+
+		//가로나 세로축으로 퍼센트로 적용될만한 속성명을 배열로 그룹화
+		const x = ['margin-left', 'margin-right', 'left', 'right', 'width'];
+		const y = ['margin-top', 'marginb-bottom', 'top', 'bottom', 'height'];
+
+		//option.prop값으로 위의 배열로 설정한 속성이 들어오면 currentValue값을 부모요소의 크기대비 퍼센트로 변환
+		for (let cond of x) {
+			if (option.prop === cond) {
+				currentValue = (currentValue / parentW) * 100;
+			}
+		}
+		for (let cond of y) {
+			if (option.prop === cond) {
+				currentValue = (currentValue / parentH) * 100;
+			}
+		}
+		option.value = parseFloat(option.value);
+	}
+
 	option.value !== currentValue && requestAnimationFrame(run);
 
 	function run(time) {
@@ -26,6 +50,9 @@ function anime(selector, option) {
 		progress < 1 ? requestAnimationFrame(run) : option.callback && option.callback();
 
 		let result = currentValue + (option.value - currentValue) * progress;
-		selector.style[option.prop] = result + 'px';
+
+		//속성값이 퍼센트이면 적용도 퍼센트 처리 그렇지 않으면 그냥 px처리
+		if (isString === 'string') selector.style[option.prop] = result + '%';
+		else selector.style[option.prop] = result + 'px';
 	}
 }
