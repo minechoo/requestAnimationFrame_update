9a8a36a3a65a9bf90dd41518eecc8bc87db685da
minechoo <choominhae@gmail.com>
2023-09-01 11:02:31 +0900

    first

--- anime.js
+++ anime.js
@@ -59,53 +59,81 @@ function anime(selector, option) {
 }
 */
 class Anime {
-	constructor(selector, option) {
+	constructor(selector, props, duration, callback) {
 		this.selector = selector;
-		this.option = option;
+		this.keys = Object.keys(props);
+		this.values = Object.values(props);
+		this.duration = duration;
+		this.callback = callback;
 		this.startTime = performance.now();
-		this.currentValue = null;
+		//props객체어서 key, value값을 배열로 뽑고 인스턴스 객체로 넘긴다음
+		//인스턴스 생성시 내부적으로 해당 배열의 값들을 setValue메서드를 반복호출하면서 인수로 전달
+		this.keys.forEach((key, idx) => this.setValue(key, this.values[idx]));
+	}
 
-		this.option.prop === 'scroll' ? (this.currentValue = this.selector.scrollY) : (this.currentValue = parseFloat(getComputedStyle(this.selector)[this.option.prop]));
-
-		this.isString = typeof this.option.value;
-		if (this.isString === 'string') {
+	setValue(key, value) {
+		console.log(key);
+		let currentValue = null;
+		const isString = typeof value;
+		if (isString === 'string') {
 			const parentW = parseInt(getComputedStyle(this.selector.parentElement).width);
 			const parentH = parseInt(getComputedStyle(this.selector.parentElement).height);
 
 			const x = ['left', 'right', 'width'];
 			const y = ['top', 'bottom', 'height'];
-			const errProps = ['margin-left', 'margin-right', 'padding-left', 'padding-right', 'margin-top', 'margin-bottom', 'padding-top', 'padding-bottom'];
+			const errProps = [
+				'margin-left',
+				'margin-right',
+				'padding-left',
+				'padding-right',
+				'margin-top',
+				'margin-bottom',
+				'padding-top',
+				'padding-bottom',
+			];
 
-			for (let cond of errProps) if (this.option.prop === cond) return console.error('margin, padding값은 퍼센트 모션처리할 수 없습니다.');
+			for (let cond of errProps)
+				if (key === cond) return console.error('margin, padding값은 퍼센트 모션처리할 수 없습니다.');
+			for (let cond of x) key === cond && (currentValue = (currentValue / parentW) * 100);
+			for (let cond of y) key === cond && (currentValue = (currentValue / parentH) * 100);
 
-			for (let cond of x) this.option.prop === cond && (this.currentValue = (this.currentValue / parentW) * 100);
-			for (let cond of y) this.option.prop === cond && (this.currentValue = (this.currentValue / parentH) * 100);
-
-			this.option.value = parseFloat(this.option.value);
+			value = parseFloat(value);
+			console.log('curVal', currentValue, 'tarVal', value);
 		}
-
-		//프로토타입에 등록되어 있는 run메서드 안쪽에서 this객체를 못 읽는 이유
-		//화살표함수 안쪽에 this객체가 있어야지 상위 코드블록의 this객체값을 참조해서 가져옴
-		//특정 메서드를 화살표 함수로 wrapping처리
-		//주의할점 - requestAnimationFrame은 직계 콜백함수에만 파라미터를 전달하기 때문에
-		//중간에 wrapping함수로 감싸주면 파라미터값을 wrapping함수에 전달되므로 해당 값을 다시 안쪽에 재 전달해줘야함
-		this.option.value !== this.currentValue && requestAnimationFrame((time) => this.run(time));
+		// key === 'scroll'
+		// 	? (this.currentValue = this.selector.scrollY)
+		// 	: (this.currentValue = parseFloat(getComputedStyle(this.selector)[this.option.prop])); //scroll은 윈도우 객체라 컴퓨티드스타일 못가져옴
 	}
-	run(time) {
-		console.log(this);
-		let timelast = time - this.startTime;
-		let progress = timelast / this.option.duration;
 
-		progress < 0 && (progress = 0);
-		progress > 1 && (progress = 1);
+	/*
+		
 
-		progress < 1 ? requestAnimationFrame((time) => this.run(time)) : this.option.callback && this.option.callback();
-
-		let result = this.currentValue + (this.option.value - this.currentValue) * progress;
-
-		if (this.isString === 'string') this.selector.style[this.option.prop] = result + '%';
-		else if (this.option.prop === 'opacity') this.selector.style[this.option.prop] = result;
-		else if (this.option.prop === 'scroll') this.selector.scroll(0, result);
-		else this.selector.style[this.option.prop] = result + 'px';
-	}
+		this.isString = typeof this.option.value;
+		
+		}
+*/
+	//프로토타입에 등록되어 있는 run메서드 안쪽에서 this객체를 못 읽는 이유
+	//화살표함수 안쪽에 this객체가 있어야지 상위 코드블록의 this객체값을 참조해서 가져옴
+	//특정 메서드를 화살표 함수로 wrapping처리
+	//주의할점 - requestAnimationFrame은 직계 콜백함수에만 파라미터를 전달하기 때문에
+	//중간에 wrapping함수로 감싸주면 파라미터값을 wrapping함수에 전달되므로 해당 값을 다시 안쪽에 재 전달해줘야함
+	//this.option.value !== this.currentValue && requestAnimationFrame((time) => this.run(time));
 }
+
+// run(time) {
+// 	console.log(this);
+// 	let timelast = time - this.startTime;
+// 	let progress = timelast / this.option.duration;
+
+// 	progress < 0 && (progress = 0);
+// 	progress > 1 && (progress = 1);
+
+// 	progress < 1 ? requestAnimationFrame((time) => this.run(time)) : this.option.callback && this.option.callback();
+
+// 	let result = this.currentValue + (this.option.value - this.currentValue) * progress;
+
+// 	if (this.isString === 'string') this.selector.style[this.option.prop] = result + '%';
+// 	else if (this.option.prop === 'opacity') this.selector.style[this.option.prop] = result;
+// 	else if (this.option.prop === 'scroll') this.selector.scroll(0, result);
+// 	else this.selector.style[this.option.prop] = result + 'px';
+// }
