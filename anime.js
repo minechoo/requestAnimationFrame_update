766c42cdd1f75334c7b454f17e176c617d403d30
minechoo <choominhae@gmail.com>
2023-09-04 13:19:26 +0900

    컬러값 준비

--- anime.js
+++ anime.js
@@ -10,13 +10,26 @@ class Anime {
 		this.easeType = this.defOpt.easeType;
 		this.startTime = performance.now();
 		this.isString = null;
+		this.isPercent = null;
+		this.isBg = null;
 		this.easingProgress = null;
 		//인스턴스 복사시 props의 갯수만큼 반복을 돌면서 속성종류에 따라 value값을 보정해주는 getValue에 반복전달
-		this.keys.forEach((key, idx) => this.getValue(key, this.values[idx]));
+		this.keys.forEach((key, idx) => {
+			//반복도는 value값이 문자열이면 다시 조건처리
+			typeof this.values[idx] === 'string'
+				? this.values[idx].includes('%')
+					? //문자열인데 %기호가 있으면 퍼센트처리
+					  this.getValue(key, this.values[idx], 'percent')
+					: //문자열인데 %기호가 없으면 색상처리
+					  this.getValue(key, this.values[idx], 'color')
+				: //문자값이 아니면 그냥 일반 연산처리
+				  this.getValue(key, this.values[idx], 'basic');
+		});
 	}
 
 	//반복돌면서 전달되는 key, value값을 활용에 속성에 맞게 값을 가공해서 run메서드에 보내줌
-	getValue(key, value) {
+	//세번째 인수로 추가전달되는 type값에 따라 각각 percent, bg, basic분기 연산처리
+	getValue(key, value, type) {
 		let currentValue = null;
 		this.isString = typeof value === 'string';
 		currentValue = parseFloat(getComputedStyle(this.selector)[key]);
@@ -24,7 +37,7 @@ class Anime {
 			? (currentValue = this.selector.scrollY)
 			: (currentValue = parseFloat(getComputedStyle(this.selector)[key]));
 
-		if (this.isString) {
+		if (type === 'percent') {
 			const parentW = parseInt(getComputedStyle(this.selector.parentElement).width);
 			const parentH = parseInt(getComputedStyle(this.selector.parentElement).height);
 			const x = ['left', 'right', 'width'];
@@ -35,21 +48,30 @@ class Anime {
 			for (let cond of y) key === cond && (currentValue = (currentValue / parentH) * 100);
 			const percentValue = parseFloat(value);
 			percentValue !== currentValue &&
-				requestAnimationFrame((time) => this.run(time, key, currentValue, percentValue, true));
-		} else {
-			value !== currentValue && requestAnimationFrame((time) => this.run(time, key, currentValue, value, false));
+				requestAnimationFrame((time) => this.run(time, key, currentValue, percentValue, type));
+		}
+		if (type === 'color') {
+			value !== currentValue && requestAnimationFrame((time) => this.run(time, key, currentValue, value, type));
+		}
+		if (type === 'basic') {
+			value !== currentValue && requestAnimationFrame((time) => this.run(time, key, currentValue, value, type));
 		}
 	}
 
 	//getValue로 전달된 값을 requestAnimationFrame으로부터 전달받아서 내부의 getProgress메서드에 전달
-	run(time, key, currentValue, value, isPercent) {
+	run(time, key, currentValue, value, type) {
 		let [progress, result] = this.getProgress(time, currentValue, value);
-		//result값을 활용해서 실제적으로 DOM에 세팅
-		this.setValue(key, result, isPercent);
-		//progress값을 활용해서 반복유무를 결정
+		//run메서드의 type을 다시 setValue로 전달
+		this.setValue(key, result, type);
 		progress < 1
-			? requestAnimationFrame((time) => this.run(time, key, currentValue, value, isPercent ? true : false))
-			: this.callback && this.callback();
+			? //진행률이 1에 도달하지 않으면 반복처리
+			  //percent, color, basic조건을 반복돌면서
+			  //run메서드를 통해서  전달되는 type값이 현재 반복도는 타입과 동일하면 해당 타입을 다시 run메서드의 마지막 인수로 전달해서 재귀적 호출
+			  ['percent', 'color', 'basic'].map(
+					(el) => type === el && requestAnimationFrame((time) => this.run(time, key, currentValue, value, type))
+			  )
+			: //1에 도달하면 반복중지하고 콜백함수 처리
+			  this.callback && this.callback();
 	}
 
 	//run메서드 안쪽에서 전달된 currentValue,value값을 가지고 속성별로 진행률과 진행률이 적용된 result값을 반환
@@ -65,8 +87,8 @@ class Anime {
 
 		Object.keys(easingPresets).map((key) => {
 			//easingPresets[key] : linear, ease1, ease2에 등록되어 있는 각각의 배열
-			//옵션으로 전달한 easeType, 내부적으로 반복도는  key값이 동잃 항목에 대한 배열에 값을 BezierEasing에 전달
-			//다시 매칭되는 배열값을 다시 순번에 맞게 뽑아서 BeizeEading에 각각 전달
+			//옵션으로 전달한 easeType과 내부적으로 반복도는  key값이 동일할때 해당 조건의 배열값을 BezierEasing에 전달
+			//매칭되는 배열값을 다시 순번에 맞게 뽑아서 BeizeEading에 각각 전달한후 반환되는 함수에 바로 progress를 인수로 전달해 호출
 			if (this.easeType === key)
 				this.easingProgress = BezierEasing(
 					easingPresets[key][0],
@@ -83,8 +105,9 @@ class Anime {
 	}
 
 	//전달된 result값으로 실제적으로 돔의 변화 세팅
-	setValue(key, result, isPercent) {
-		if (isPercent) this.selector.style[key] = result + '%';
+	setValue(key, result, type) {
+		if (type === 'percent') this.selector.style[key] = result + '%';
+		else if (type === 'color') this.selector.style[key] = result + '%';
 		else if (key === 'opacity') this.selector.style[key] = result;
 		else if (key === 'scroll') this.selector.scroll(0, result);
 		else this.selector.style[key] = result + 'px';
